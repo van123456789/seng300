8db2a2949e18cfc44be4109bacbb3209cdc1b794
van <wadvan987@gmail.com>
2019-05-26 23:21:51 -0600

    add authenticator, some test cases for users, frame for other classes

--- seng300/src/seng300/Main.java
+++ seng300/src/seng300/Main.java
@@ -1,25 +1,55 @@
 package seng300;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
+		String combination;
+		combination = getInput();
+		checker(combination);
+	}
+	
+	private static boolean checker(String combination) {
+		boolean toReturn = false;
+		id_checker checker = new id_checker(combination);
+		int id_type = checker.authenticate();
+		if (id_type == 1) {
+			System.out.println("Student found");
+			toReturn = true;
+		}
+		if (id_type == 2) {
+			System.out.println("Instructor found");
+			toReturn = true;
+		}
+		if (id_type == 3) {
+			System.out.println("Head Department found");
+			toReturn = true;
+		}
+		if (id_type == -1) {
+			System.out.println("User not found");
+		}
+		
+		
+		return toReturn;
+	}
+	
+	private static String getInput() {
 		System.out.println("Enter your unique combination to continue: ");
 		Scanner myScanner = new Scanner(System.in);
+		String unique_combination = "Error";
 		try {
-			int unique_combination = myScanner.nextInt();
+			unique_combination = myScanner.nextLine();
 		}
 		catch (InputMismatchException e) {
 			System.out.println("Invalid input");
 		}
-	}
-	
-	private boolean checker(int combination) {
-		boolean toReturn = false;
 		
-		
-		
-		return toReturn;
+		return unique_combination;
 	}
 
 }
---
+++ seng300/src/seng300/id_checker.java
@@ -0,0 +1,103 @@
+package seng300;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class id_checker {
+	private String id;
+	private int id_type = -1;
+	public id_checker(String combination) {
+		id = combination;
+	}
+	
+	private void check() {
+		boolean isStudent = checkStudent();
+		boolean isInstructor = checkInstructor();
+		boolean isHead = checkHead();
+		
+		if (isStudent) {
+			id_type = 1;
+		}
+		if (isInstructor) {
+			id_type = 2;
+		}
+		if (isHead) {
+			id_type = 3;
+		}
+		
+	}
+	
+	private boolean checkStudent() {
+		boolean toReturn = false;
+		try {
+			FileReader headdep = new FileReader("headdepartment.data");
+			BufferedReader headdep_reader = new BufferedReader(headdep);
+			
+			String temp;
+			while ((temp = headdep_reader.readLine()) != null) {
+				if (temp.equals(id)){
+					toReturn = true;
+				}
+			}
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		}
+		catch (IOException e) {
+			System.out.println("IO error");
+		}
+		return toReturn;
+	}
+	
+	private boolean checkInstructor() {
+		boolean toReturn = false;
+		try {
+			FileReader instructor = new FileReader("instructor.data");
+			BufferedReader instructor_reader = new BufferedReader(instructor);
+			
+			String temp;
+			while ((temp = instructor_reader.readLine()) != null) {
+				if (temp.equals(id)){
+					toReturn = true;
+				}
+			}
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		}
+		catch (IOException e) {
+			System.out.println("IO error");
+		}
+		return toReturn;
+	}
+	
+	private boolean checkHead() {
+		boolean toReturn = false;
+		try {
+			FileReader student = new FileReader("student.data");
+			BufferedReader student_reader = new BufferedReader(student);
+			
+			String temp;
+			while ((temp = student_reader.readLine()) != null) {
+				if (temp.equals(id)){
+					toReturn = true;
+				}
+			}
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		}
+		catch (IOException e) {
+			System.out.println("IO error");
+		}
+		return toReturn;
+	}
+	
+	public int authenticate() {
+		this.check();
+		return id_type;
+	}
+
+}
