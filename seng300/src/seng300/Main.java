c2ab77d9e3abf7541846e1acc28c32102f0d80e2
Changho Lee <yohan9403@gmail.com>
2019-06-03 23:17:32 -0600

    added checker back just in case

--- seng300/src/seng300/Main.java
+++ seng300/src/seng300/Main.java
@@ -10,7 +10,97 @@ public class Main {
 
 	
 	public static void main(String[] args) throws Exception 
-	{	
+	{
+	
 		new Login();
+/*
+		String combination;
+		combination = getInput();
+		int id_type = checker(combination);
+		if (id_type == 1) {
+			runAsStudent(combination);
+		}
+		if (id_type == 2) {
+			runAsInstructor(combination);
+		}
+		if (id_type == 3) {
+			runAsHeadDep(combination);
+		}
+*/				
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+/*	
+	private static void runAsStudent(String combination) {
+		Student aStudent = new Student(combination);
+		aStudent.run();
+	}
+	private static void runAsInstructor(String combination) {
+		
+	}
+	private static void runAsHeadDep(String combination) {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private static int checker(String combination) {
+		int toReturn = -1;
+		id_checker checker = new id_checker(combination);
+		int id_type = checker.authenticate();
+		if (id_type == 1) {
+			System.out.println("Student found");
+			toReturn = 1;
+		}
+		if (id_type == 2) {
+			System.out.println("Instructor found");
+			toReturn = 2;
+		}
+		if (id_type == 3) {
+			System.out.println("Head Department found");
+			toReturn = 3;
+		}
+		if (id_type == -1) {
+			System.out.println("User not found");
+		}
+		
+		
+		return toReturn;
+	}
+	
+	private static String getInput() {
+		System.out.println("Enter your unique combination to continue: ");
+		Scanner myScanner = new Scanner(System.in);
+		String unique_combination = "Error";
+		try {
+			unique_combination = myScanner.nextLine();
+		}
+		catch (InputMismatchException e) {
+			System.out.println("Invalid input");
+		}
+		return unique_combination;
+	}
+*/
 }
