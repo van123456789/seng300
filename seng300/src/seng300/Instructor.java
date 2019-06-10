2162188a14a44a1cdd051dd380562d4cd92642da
van <wadvan987@gmail.com>
2019-06-10 10:48:04 -0600

    aaaaaaaaaaaaaaaa

--- seng300/src/seng300/Instructor.java
+++ seng300/src/seng300/Instructor.java
@@ -13,6 +13,10 @@ import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 
 
 public class Instructor extends User{
@@ -27,30 +31,40 @@ public class Instructor extends User{
 		this.teaching = new ArrayList<Course>();
 	}
 	
+	public Instructor (String id) {
+		super.id = id;
+	}
 	
-	public void run() {
-		System.out.println("Enter your action:");
-		System.out.println("Type 1 to request to teach a course");
-		System.out.println("Type 2 to view current request's result");
-		Scanner myScanner = new Scanner(System.in);
-		String action = null;
-		boolean notDone = true;
-		while (notDone) {
-			action = myScanner.nextLine();
-			if (action.equals("1") || action.equals("2")) {
-				notDone = false;
-			}
-			else {
-				System.out.println("Incorrect input");
-			}
-		}
-		
-		
-		if (action.equals("1")) {
-			requestcourse();
-		}
-		else {
-			reviewrequest();
+	public void run() {		
+		boolean stillOn = true;
+		while (stillOn) {
+			System.out.println("Enter your action:");
+			System.out.println("Type 1 to request to teach a course");
+			System.out.println("Type 2 to view current request's result");
+			System.out.println("Type 3 to exit the system");
+			
+
+			Scanner sc = new Scanner(System.in);
+			String user_choice = sc.nextLine();
+			
+			switch(user_choice) 
+			{
+				case "1":
+					requestcourse();
+					break;
+
+				case "2":
+					reviewrequest();
+					break;
+					
+				case "3":
+					stillOn = false;
+					break;
+	
+				default:
+					System.out.println("invalid option");
+					break;
+			}		
 		}
 	}
 	
@@ -71,36 +85,39 @@ public class Instructor extends User{
 				System.out.println("Invalid type input");
 			}
 		}
-		System.out.println("These are unassigned courses");
-		print_unassigned_courses();
-		System.out.println("Please enter course you want to teach, type exit to stop adding");
-		get_request();
+		System.out.println("These are unassigned courses available: ");
+		print_unassigned_courses(input);
+		get_request(input);
 		
 	}
 	
 	
-	public void get_request() {
+	public void get_request(String aSession) {
 		boolean isFinished = false;
 		Scanner myScanner = new Scanner(System.in);
 		String input = null;
-		List<String> courses = this.getUnassigned_courses();
+		List<String> unassigned_courses = this.getUnassigned_courses(aSession);
 		List<String> requesting_courses = new ArrayList<>();
 		int state = 1;
 		
 		while (!isFinished) {
-			input = myScanner.nextLine();
+			
 			
 			if (state == 1) {
-				if (courses.contains(input)) {
-					requesting_courses.add(input); 
+				System.out.println("Please type in the course you want to request");
+				System.out.println("Please type exit to stop adding");
+				input = myScanner.nextLine();
+				if (unassigned_courses.contains(input)) {
+					requesting_courses.add(input);
+					System.out.println("Courses you are requesting: ");
 					System.out.println(requesting_courses);
 				}
 				if (input.contentEquals("exit")) {
 					state = 2;
 				}
 				
-				else {
-					System.out.println("Invalid Input");
+				if (!unassigned_courses.contains(input) && !input.equals("exit")) {
+					System.out.println("Invalid input");
 				}
 			}
 			
@@ -109,26 +126,44 @@ public class Instructor extends User{
 				System.out.println(requesting_courses);
 				System.out.println("Type ok to confirm this request");
 				System.out.println("Type remove to remove courses in the list");
+				input = myScanner.nextLine();
 				
-				if (input.contentEquals("ok")) {
-					send_request(requesting_courses);
-				}
-				if (input.contentEquals("remove")) {
-					boolean isDone = false;
-					while (!isDone) {
-						System.out.println("Please enter course name you want to remove");
-						System.out.println("Please type exit to exit");
-						input = myScanner.nextLine();
-						
-						if (requesting_courses.contains(input)) {
-							requesting_courses.remove(input);
-							System.out.println(requesting_courses);
+				
+				switch(input) {
+					case "ok":
+						isFinished = true;
+						send_request(requesting_courses);
+						break;
+					
+					case "remove":
+						boolean isDone = false;
+						while (!isDone) {
+							System.out.println("Please enter course name you want to remove");
+							System.out.println("Please type exit to exit");
+							input = myScanner.nextLine();
+							
+							if (!requesting_courses.contains(input) && !input.equals("exit")) {
+								System.out.println("Cant find the mentioned course");
+							}
+							
+							if (requesting_courses.contains(input)) {
+								requesting_courses.remove(input);
+								System.out.println("Removed successfully, requesting courses are:");
+								System.out.println(requesting_courses);
+								
+							}
+							if (input.equals("exit")) {
+								isDone = true;
+							}
+							
+							
 							
 						}
-						else {
-							System.out.println("Cant find the mentioned course");
-						}
-					}
+						break;
+						
+					default:
+						System.out.println("Invalid option");
+						break;
 					
 				}
 			}
@@ -141,30 +176,40 @@ public class Instructor extends User{
 		
 	}
 	
-	public void print_unassigned_courses() {
-		System.out.println(this.getUnassigned_courses());
+	public void print_unassigned_courses(String aSession) {
+		System.out.println(this.getUnassigned_courses(aSession));
 	}
 	
-	public List<String> getUnassigned_courses(){
-		List<String> courses = new ArrayList<>();
-		try {
-			FileReader courselist = new FileReader("courselist.data");
-			BufferedReader courselist_reader = new BufferedReader(courselist);
-			
-			
-			String temp;
-			while ((temp = courselist_reader.readLine()) != null) {
-				String[] splitedLine = temp.split("-");
-				if (splitedLine[2].contentEquals("")) {
-					courses.add(splitedLine[1]);
-				}
-			}
-		}
-		
-		catch (Exception e) {
-			System.out.println("Error in print_unassigned_course");
-		}
-		return courses;
+	public List<String> getUnassigned_courses(String aSession){
+		ArrayList<String> result = new ArrayList<>();
+		JSONParser jsonParser = new JSONParser();
+		ArrayList <String> courseload = new ArrayList<>();
+        
+        try (FileReader reader = new FileReader("test1.json")) {
+        	
+            Object obj = jsonParser.parse(reader);
+            JSONArray courseList = (JSONArray) obj;
+            
+			JSONObject courseDetails=new JSONObject();
+            for (int i = 0; i < courseList.size(); i++) {
+            	courseDetails = (JSONObject) courseList.get(i);
+            	String instructor = (String) courseDetails.get("instructor");
+            	String session = (String) courseDetails.get("session");
+            	String coursename = (String) courseDetails.get("coursename");
+            	if (session.equals(aSession) && instructor.equals("")) {
+            		result.add(coursename);
+            	}
+            }
+            
+
+        }
+        catch (Exception e) {
+        	System.out.println("Error in print_course_load");
+        }
+        
+        return result;
+        
+	
 	}
 		
 	
@@ -173,35 +218,36 @@ public class Instructor extends User{
 	}
 	
 	public static void main(String[] args) {
-		Instructor aInstructor = new Instructor("7643541004");
-		aInstructor.run();
+		//Instructor aInstructor = new Instructor("7643541004");
+		//aInstructor.run();
+		//Instructor.inititialize();
+	}
+	
+	public static void inititialize() {
+		
+		
+		JSONObject aRequest = new JSONObject();
+        aRequest.put("instructor", "7643541004");
+        ArrayList<String> requestingList = new ArrayList<>();
+        requestingList.add("91903");
+        requestingList.add("47326");
+        aRequest.put("course_id", requestingList.toString());
+         
+
+        JSONArray request_total_list = new JSONArray();
+        request_total_list.add(aRequest);
+         
+        //Write JSON file
+        try (FileWriter file = new FileWriter("requesting.json")) {
+ 
+            file.write(request_total_list.toJSONString());
+            file.flush();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
 
 
-	public void run(String id) 
-	{
-		
-		System.out.println("Please select your option:");
-		System.out.println("1 - request to teach a course");
-		System.out.println("2 - request to remove a course");
-		System.out.println("3 - update course information");
-		
-		sc = new Scanner(System.in);
-		
-		while (true) {
-			user_choice = sc.nextLine();
-			if (user_choice.equals("1")) {
-				break;
-			}
-			if (user_choice.equals("2")) {
-				break;
-			}
-			if (user_choice.equals("3")) {
-				break;
-			}			
-			else {
-				System.out.println("Incorrect response");
-			}
-		}
-	}		
+	
 }
